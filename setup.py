d6de0d2fffae8f6b6687e50ba8328fc579f43500
Dan Poland <dan@crispy.dev>
2020-03-25 09:23:10 -0400

    Realase 1.2.1 bump.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="slacktools-interactivity",
-    version="1.2.0",
+    version="1.2.1",
     author="Daniel Poland",
     author_email="dan@crispy.dev",
     long_description=long_description,
